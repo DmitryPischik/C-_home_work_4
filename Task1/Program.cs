8f2f8416b3cae35a9d3800e8fc56ef05463c5334
Dmitriy Pischik <pischik@bk.ru>
2023-01-24 20:47:48 +1000

    Added folder Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] new_array(int number) // функция задаёт массив с заданным кол-м случайных значений.
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(100, 1000); 
+    }
+    return array;
+}
+void PrintArr(int[] array) // функция просматривает массив целых часел.
+{  
+    foreach (int item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+void PosNegCount(int[] array) // функция показывает количество нечетных\четных чисел.
+{  
+    int PosCount = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0) PosCount++;
+    }
+    Console.WriteLine($"В массиве {PosCount} чётных чисел");
+}
+//////////////
+
+Console.Write("Укажите число элементов для нового массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new_array(n);
+Console.WriteLine("Посмотрим сгенерированный массив:");
+PrintArr(arr);
+PosNegCount(arr);
