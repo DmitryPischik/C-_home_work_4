84d7d831be68ff7cea1ec2f11f38a1cf408e09ac
Dmitriy Pischik <pischik@bk.ru>
2023-01-24 20:52:15 +1000

    Added folder Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] new_array(int number) // функция задаёт массив с заданным кол-м случайных значений.
+{
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100); 
+    }
+    return array;
+}
+void PrintArr(int[] array) // функция просматривает массив целых часел.
+{  
+    foreach (int item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+void NegSumCount(int[] array) // функция показывает сумму чисел одномерного массива стоящих на нечетной позиции.
+{  
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum = sum + array[i];
+    }
+    Console.WriteLine($"Сумма чисел одномерного массива стоящих на нечетной позиции = {sum}");
+}
+//////////////
+
+Console.Write("Укажите число элементов для нового массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new_array(n);
+Console.WriteLine("Посмотрим сгенерированный массив:");
+PrintArr(arr);
+NegSumCount(arr);
